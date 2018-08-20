e0ce19e54046a17d8ceaf6d134a4ca9b4fbc112a
OAZZZIZ <o@zzz.iz>
2018-08-20 23:48:21 +0300

    changes

--- Enqm/API/RPC/Instances.hs
+++ Enqm/API/RPC/Instances.hs
@@ -29,6 +29,13 @@ instance Show ApiRpcMethods where
 class Test f where
   cnm :: f a -> String
 
+data Kuku
+  = Kuku { n220 :: Int, v :: Double }
+  | CDSc { cdcd :: Integer, cdwcd :: String }
+  | DDDD Int
+ deriving (Generic)
+
+
 instance Test V1 where cnm _  = "V1"
 instance Test U1 where cnm U1 = "U1"
 
@@ -37,16 +44,22 @@ instance (Test f, Test g) => Test (f :+: g) where
   cnm (R1 x) = cnm x
   
 instance (Test f, Test g) => Test (f :*: g) where
-  cnm (x :*: y) = ":*:"
+  cnm (x :*: y) = "(" ++ cnm x ++ ":*:" ++ cnm y ++ ")"
 
 instance (Typeable c) => Test (K1 R c) where
   cnm _ = "(" ++ (show $ typeOf (undefined :: c)) ++ ")"
 
 instance {-# OVERLAPPABLE #-} (Test f)  => Test (M1 i t f) where
-  cnm = cnm . unM1
+  cnm a = cnm (unM1 a)
 
-instance {-# OVERLAPPING #-} (Test f,KnownSymbol a)  => Test (M1 i ('MetaCons a b c) f) where
+instance {-# OVERLAPPING #-} (Test f, KnownSymbol a)  => Test (M1 i ('MetaCons a b c) f) where
   cnm a = symbolVal (Proxy :: Proxy a) ++ " " ++ (cnm $ unM1 a)
+
+instance {-# OVERLAPPING #-} (Test f, KnownSymbol a)  => Test (M1 i ('MetaSel ('Just a) b c d) f) where
+  cnm a = symbolVal (Proxy :: Proxy a) ++ " " ++ (cnm $ unM1 a)
+
+instance {-# OVERLAPPING #-} (Test f)  => Test (M1 i ('MetaSel ('Nothing) b c d) f) where
+  cnm a = "field " ++ (cnm $ unM1 a)
 
 
 test :: (Test (Rep a), Generic a) => a -> String
